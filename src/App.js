5ae5e6f23ff6387ade492c5e69e91920259bc2b8
Nicholas Andrew Miller <nick.miller@govlawtech.com.au>
2018-06-18 19:40:51 +1000

    first react thing working

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import CurrentTempComponent from "./components/weather";
+import BomWeatherComponent from "./components/weather";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-          <CurrentTempComponent temp="10"/>
-
+        <BomWeatherComponent/>
       </div>
     );
   }
--- src/components/weather.js
+++ src/components/weather.js
@@ -3,12 +3,61 @@ import React, {Component} from 'react'
 
 // call this: http://reg.bom.gov.au/fwo/IDN60903/IDN60903.94926.json
 
+const getBomReport = () => new Promise((resolves,rejects) => {
+    const endPoint = "http://reg.bom.gov.au/fwo/IDN60903/IDN60903.94926.json"
+    const request = new XMLHttpRequest()
+    request.open('GET',endPoint)
+    request.onload = () => (request.status == 200) ?
+        resolves(JSON.parse(request.response)) :
+        rejects(Error(request.statusText))
+    request.onerror = err => rejects(err)
+    request.send()
 
-const CurrentTempComponent = ({temp}) => (
-    <div>
-        <p>Current: {temp}</p>
-    </div>
-)
+})
 
 
-export default CurrentTempComponent
+class BomWeatherComponent extends Component {
+
+
+    constructor() {
+        super()
+        this.state = {
+            loading: false,
+            bom: {}
+        }
+    }
+
+
+
+    componentWillMount() {
+        this.setState({loading: true})
+        getBomReport().then(
+            results => {
+                const latestObservation = results.observations.data[0]
+                //const temp = results.observations.data[0].air_temp
+                this.setState({bom: latestObservation, loading: false})
+            },
+            error => {
+                this.setState({currentTemp: "error", loading: false})
+            }
+        )
+    }
+
+    render() {
+
+        const currentTemp = this.state.bom.air_temp
+
+        return (
+            <div>
+                <p>Current: {currentTemp}</p>
+            </div>
+        )
+
+    }
+
+}
+
+
+
+
+export default BomWeatherComponent
