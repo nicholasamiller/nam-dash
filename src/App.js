79237ecd02e807394bd9a0f86c861ee0fc361a7f
Nicholas Andrew Miller <nick.miller@govlawtech.com.au>
2018-06-22 15:53:53 +1000

    wip

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 import BomWeatherComponent from "./components/weather";
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <BomWeatherComponent/>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <div className="container-fluid">
+                    <div className="row">
+                        <div className="col-sm">
+                            <BomWeatherComponent/>
+                        </div>
+                        <div className="col-sm">
+
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/components/weather.js
+++ src/components/weather.js
@@ -7,13 +7,14 @@ const getBomReport = () => new Promise((resolves,rejects) => {
     const endPoint = "http://reg.bom.gov.au/fwo/IDN60903/IDN60903.94926.json"
     const request = new XMLHttpRequest()
     request.open('GET',endPoint)
-    request.onload = () => (request.status == 200) ?
+    request.onload = () => (request.status === 200) ?
         resolves(JSON.parse(request.response)) :
         rejects(Error(request.statusText))
     request.onerror = err => rejects(err)
     request.send()
 
 })
+
 
 
 class BomWeatherComponent extends Component {
@@ -28,28 +29,63 @@ class BomWeatherComponent extends Component {
     }
 
 
-
-    componentWillMount() {
-        this.setState({loading: true})
+    refreshBom = () => {
         getBomReport().then(
             results => {
                 const latestObservation = results.observations.data[0]
                 //const temp = results.observations.data[0].air_temp
-                this.setState({bom: latestObservation, loading: false})
+                this.setState({bom: latestObservation})
             },
             error => {
-                this.setState({currentTemp: "error", loading: false})
+                this.setState({bom: "error"})
             }
         )
+
+    }
+
+
+
+    componentWillMount() {
+        this.setState({loading: true})
+        this.refreshBom()
+        this.setState({loading:false})
+    }
+
+    componentDidMount() {
+
+        this.interval = setInterval(() => this.refreshBom(),60000)
+    }
+    componentWillUnmount() {
+        clearInterval(this.interval);
     }
 
     render() {
 
         const currentTemp = this.state.bom.air_temp
+        const cloudType = this.state.bom.cloud_type
+        const windDir = this.state.bom.wind_dir
+        const windSpeed = this.state.bom.wind_spd_kmh
+        const lastReport = this.state.bom.local_date_time
+
 
         return (
             <div>
-                <p>Current: {currentTemp}</p>
+                <table className="table-sm table-dark">
+                    <tbody>
+                        <tr>
+                            <th scope="row">Current temp</th><td>{currentTemp} °C</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">Cloud type</th><td>{cloudType}</td>
+                        </tr>
+                        <tr>
+                          <th scope="row">Wind</th><td>{windDir} {windSpeed} kph</td>
+                        </tr>
+                        <tr>
+                            <td>Last updated</td><td>{lastReport}</td>
+                        </tr>
+                    </tbody>
+                </table>
             </div>
         )
 
